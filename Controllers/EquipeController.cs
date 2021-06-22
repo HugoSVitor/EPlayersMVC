5627898bbc91e6e115e4f8606b30f812fb775aa7
Hugo da Silva Vitor <hugodasilvav0209@gmail.com>
2021-06-21 23:28:30 -0300

    21-06 dia finalizado

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using EPlayersMVCEx.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -23,12 +24,48 @@ namespace EPlayersMVCEx.Controllers
             Equipe novaEquipe = new Equipe();
             novaEquipe.IdEquipe = Int32.Parse(form["IdEquipe"]);
             novaEquipe.Nome = form["Nome"];
-            novaEquipe.Imagem = form["Imagem"];
+            //novaEquipe.Imagem = form["Imagem"];
+
+            //Upload Inicio
+
+            if (form.Files.Count > 0)
+            {
+                var file = form.Files[0];
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                novaEquipe.Imagem = file.FileName;
+
+            }else
+            {
+                novaEquipe.Imagem = "padrao.png";
+            }
+
+            //Upload Final
 
             equipeModel.Criar(novaEquipe);
 
             ViewBag.Equipes = equipeModel.LerTodas();
 
+            return LocalRedirect("~/Equipe/Listar");
+        }
+
+        [Route("Excluir/{id}")]
+        public IActionResult Excluir(int id)
+        {
+            equipeModel.Deletar(id);
+            ViewBag.Equipes = equipeModel.LerTodas();
             return LocalRedirect("~/Equipe/Listar");
         }
     }
